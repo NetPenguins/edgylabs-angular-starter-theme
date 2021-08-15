167747355c97b11020cbcb35c8c73df29fdf1e67
NetPenguins <cwils192@uncc.edu>
2021-08-15 09:38:41 -0400

    additional theme editing to include device preference default.

--- src/app/services/ui-style-toggle.service.ts
+++ src/app/services/ui-style-toggle.service.ts
@@ -4,28 +4,38 @@ import {StorageService} from "./local-storage.service";
 
 
 export enum ThemeMode {
-  DARK, LIGHT
+  DEFAULT, DARK, LIGHT
 }
 
 @Injectable()
 export class UiStyleToggleService {
 
-  public theme$ = new BehaviorSubject<ThemeMode>(ThemeMode.LIGHT);
+  public theme$ = new BehaviorSubject<ThemeMode>(ThemeMode.DEFAULT);
   private readonly THEME_KEY = 'THEME';
   private readonly DARK_THEME_VALUE = 'DARK';
   private readonly LIGHT_THEME_VALUE = 'LIGHT';
   private readonly DARK_THEME_CLASS_NAME = 'theme-dark';
   private darkThemeSelected = false;
 
-  constructor(private storage: StorageService) {
+  constructor(private storage: StorageService) {}
 
-  }
-
+  /*
+    If the browser supports matchMedia and the user hasnt specified a theme, 
+    we will use prefers-color-scheme to determine which theme to default to.
+  */
   public setThemeOnStart() {
     if (this.isDarkThemeSelected()) {
       this.setDarkTheme();
-    } else {
+    } else if (this.isLightThemeSelected()) {
       this.setLightTheme();
+    }
+    if(window.matchMedia && this.isDefaultThemeSelected()) {
+      window.matchMedia('(prefers-color-scheme: dark)').matches 
+        ? this.setDarkTheme()
+        : this.setLightTheme();
+    }
+    else if(!window.matchMedia){
+      this.setDarkTheme();
     }
     setTimeout(() => {
       document.body.classList.add('animate-colors-transition');
@@ -43,6 +53,12 @@ export class UiStyleToggleService {
   public isDarkThemeSelected(): boolean {
     this.darkThemeSelected = this.storage.get(this.THEME_KEY) === this.DARK_THEME_VALUE;
     return this.darkThemeSelected;
+  }
+  public isLightThemeSelected(): boolean {
+    return this.storage.get(this.THEME_KEY) === this.LIGHT_THEME_VALUE;
+  }
+  public isDefaultThemeSelected(): boolean {
+    return this.storage.get(this.THEME_KEY) === "DEFAULT";
   }
 
   private setLightTheme() {
